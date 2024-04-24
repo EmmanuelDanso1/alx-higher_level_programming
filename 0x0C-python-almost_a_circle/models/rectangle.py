a259261bca8e8a9854c5c362ba03497702f09669
emmanueldanso1 <kwekudanso21@gmail.com>
2024-04-24 05:49:59 -0700

    new inheritance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''Rectangle module'''
-from models.base import Base
+from .base import Base
 class Rectangle(Base):
     '''Rectangle class'''
     def __init__(self, width, height, x=0, y=0, id=None):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle
+from .rectangle import Rectangle
 '''Rectangle module'''
 class Square(Rectangle)
     '''class inheritance'''
