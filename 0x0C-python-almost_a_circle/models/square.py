fb62bc3a45e92959a9c68a4ee9bcd5f448cb3d68
emmanueldanso1 <kwekudanso21@gmail.com>
2024-04-25 19:22:01 -0700

    public method assigns attribute

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,4 +18,32 @@ class Square(Rectangle)
 
     def __str__(self):
         '''Return a string representation'''
-        return "[Square]({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square]({}) {}/{} - {}".format(self.id, self.x, self.y, self.width
+
+    def update(self, *args, **kwargs):
+        '''public methods that assign attribute'''
+        if args:
+            for count, arg in enumerate(args):
+                if count == 0:
+                    self.id = arg
+                elif count == 1:
+                    self.size = arg
+                elif count == 2:
+                    self.x = arg
+                elif count == 3:
+                    self.y = arg
+                else:
+                    break
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.height = value
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+                else:
+                    break
