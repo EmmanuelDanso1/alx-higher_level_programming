6f8bc423182415a08f31974f8447f13050a8539f
emmanueldanso1 <kwekudanso21@gmail.com>
2024-05-24 04:54:24 -0700

    sorting and appendiding

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,10 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     keys = []
     if a_dictionary:
         for k, v in a_dictionary.items():
             keys.append(k)
+
         keys.sort()
         for items in keys:
             print("{}: {}".format(items, a_dictionary[items]))
