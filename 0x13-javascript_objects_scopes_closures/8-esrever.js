f1cac86248107c8513059b82cd001af314d2fa68
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-06-21 09:10:14 -0700

    returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
+// return the reverse version of a list
 exports.esrever = function (list){
     let reversed_list = [];
-    for (let i = list.lenght - 1; i >= 0; i--){
+    for (let i = list.length - 1; i >= 0; i--){
         reversed_list.push(list[i]);
     }
     return reversed_list;
