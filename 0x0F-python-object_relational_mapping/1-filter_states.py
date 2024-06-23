d7203eef19567f75859bab3f7409b9499b5fc09c
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-06-23 11:42:58 -0700

    filter by state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
+
 """
-This script that lists all `states` with a name starting
-with `N` from database `hbtn_0e_0_usa`.
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+This module connects python script to a database.
+This script lists all states with a name starting with N.
+Script should take 3 arguments:
+mysql username, mysql password and database name
+script should connect to a MySQL server running on localhost at port 3306
 """
 
+import MySQLdb as db
 from sys import argv
-import MySQLdb
 
-if __name__ == "__main__":
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(user=username, passwd=password, db=db_name)
-    cur = db.cursor()
+if __name__ == '__main__':
+    my_db = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    my_cursor = my_db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    rows = cur.fetchall()
+    my_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
 
-    for row in rows:
+    rows_selected = my_cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
