22d57698007afacc65fc1a32a82bb53756d404b8
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-06-23 10:37:37 -0700

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,49 +1,19 @@
+#!/usr/bin/python3
 """
 This is an object relational mapping project with
 MySQL and Python
 A script that lists all states from the database hbtn_0e_0_usa
 """
-import MySQLdb  # Import the MySQLdb module for MySQL database interaction
-import sys      # Import the sys module for system-related functionality
+import MySQLdb
+import sys
 
-
-def list_states(mysql_user, mysql_password, db_name):
-        # using try and except handlers to catch errors
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_user,
-            passwd=mysql_password,
-            db=db_name)
-        # Create a cursor object to interact
-        cur = db.cursor()
-        # Construct the SQL query to retrieve states sorted by ID
-        query = "SELECT * FROM states ORDER BY states.id ASC"
-        # Execute the query
-        cur.execute(query)
-        # Fetching all the rows returned by the query
-        rows = cur.fetchall()
-        # Display the results
-        for row in rows:
-            print(row)
-        # Close the cursor and database connection
-        cur.close()
-        db.close()
-    except MySQLdb.Error as e:
-        # print error if any and exit
-        print("MySQL Error:", e)
-        sys.exit(1)
 if __name__ == "__main__":
-    # Check if the correct number of command-line
-    #  arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py mysql_user mysql_password db_name")
-        sys.exit(1)
-    # Getting uname,pswd and db from commandline
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    # Caling the list function
-    list_states(mysql_user, mysql_password, db_name)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2],db=sys.argv[3],port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(rows)
+    cur.close()
+    db.close()
