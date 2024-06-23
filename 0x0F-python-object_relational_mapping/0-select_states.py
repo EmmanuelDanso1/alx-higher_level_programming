7734ad347eacf682330c4e5b2df7ca2086f7c141
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-06-23 11:06:27 -0700

    updated ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,19 +6,18 @@ Arguments:
     mysql password (str)
     database name (str)
 """
-import MySQLdb
+
 import sys
-def list_all_state(username, password, database_name):
-    db = MySQLdb.connect(host="localhost", user=username,
-                         passwd=password,db=database_name,port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(rows)
-    cur.close()
-    db.close()
+import MySQLdb
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    db = MySQLdb.connect(user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
