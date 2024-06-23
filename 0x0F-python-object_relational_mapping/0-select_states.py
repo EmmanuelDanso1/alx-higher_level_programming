a389806ff3bb64b783365d698e39c1d476f66c74
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-06-23 10:01:20 -0700

    new update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-
+"""
+connect to database using MySQLdb
+"""
 import MySQLdb
 import sys
 
-def list_all_states(username, password, dbname):
-
+if __name__ == "__main__":
+    # connecting to MySQLdb database
+    db = MySQLdb.connect(host="localhost", user="sys.argv[1]", passwd="sys.argv[2]",db="sys.argv[3]",port=3306)
     
-    if __name__ == "__main__":
-        
-        # connecting to MySQLdb database
-        db = MySQLdb.connect(host="localhost", user="sys.argv[1]", passwd="sys.argv[2]",db="sys.argv[3]",port=3306)
+    # creating cursor object to interact with database
+    cur = db.cursor()
     
-        # creating cursor object to interact with database
-        cur = db.cursor()
+    # executing query to list all states
+    cur.execute("SELECT * FROM states ORDER_BY id ASC")
     
-        # executing query to list all states
-        cur.execute("SELECT * FROM states ORDER_BY id ASC")
+    # fetching all from execute query
+    rows = cur.fetchall()
     
-        # fetching all from execute query
-        rows = cur.fetchall()
+    # looping through rows and printing eachone
+    for row in rows:
+        print(row)
     
-        # looping through rows and printing eachone
-        for row in rows:
-            print(row)
-    
-        # cleaning up the cursor and database
-        cur.close()
-        db.close()
+    # cleaning up the cursor and database
+    cur.close()
+    db.close()
+     
     
