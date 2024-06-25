26d27c0d5c3f6effb95e045b0c72b03a91109888
emmanueldanso1 <kwekudanso21@gmail.com>
2024-06-25 05:12:05 -0700

    python file that contains the class definition of a State and an instance Base = declarative_base()

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,36 +1,21 @@
-#!/usr/bin/pythob3
+#!/usr/bin/python3
 """
-This script lists all lists all `State`
-objects from the database `hbtn_0e_6_usa`.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+ State module
+ contains the state class that inherits from import declarative_base
 """
-import sys
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.engine.url import URL
 
-Base =  declarative_base()
+Base = declarative_base()
 class State(Base):
+    """
+    class links to the `states` table
+    Attributes:
+        id(int): id of state
+        name(str): name of state
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-if __name__=="__main__":
-     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-     url = {
-             "drivername": "mysql+mysqldb",
-             "host": "localhost"
-             "username": username,
-             "password":password,
-             "db": db_name,
-             "port": 3306
-             }
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_engine(engine)
 
