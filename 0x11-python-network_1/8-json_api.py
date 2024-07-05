de02783b3261fa0430bd5b35a69ca6e4a75439e4
emmanueldanso1 <kwekudanso21@gmail.com>
2024-07-05 16:48:19 -0700

    Json and API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-Scripting for Json and API
+Script which emphasizes on JSON and API
 """
-import requests
 import sys
+import requests
 
-if __name__=="__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    the_letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    data = {'q': the_letter}
-    response = requests.post(url, data)
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) < 2 else sys.argv[1]
+
+    data = {"q": letter}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
     try:
-        json_response = response.json()
-        if json_response:
-            print(f"[{json_response.get('id')}]{json_response.get('name')}")
-        else:
+        response = req.json()
+        if response == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
