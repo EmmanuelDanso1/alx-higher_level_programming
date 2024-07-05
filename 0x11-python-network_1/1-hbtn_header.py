eda292be6207518e2b83d6d7ef648028bbe77035
emmanueldanso1 <kwekudanso21@gmail.com>
2024-07-05 09:54:39 -0700

    using sys.argv to retrieve url updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,9 +7,11 @@ the value of the X-Request-Id variable found in the header of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
+if __name__ == "__main__":
 
-with urllib.request.urlopen(url) as response:
-    headers = response.headers
-    x_request_id = headers.get('X-Request-Id')
+    url = sys.argv[1]
 
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        x_request_id = headers.get('X-Request-Id')
+        print(x_request_id)
