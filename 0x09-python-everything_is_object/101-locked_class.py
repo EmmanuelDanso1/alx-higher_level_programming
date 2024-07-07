f7b7c063ca6113fa0b13508e4f743447908dea21
emmanueldanso1 <kwekudanso21@gmail.com>
2024-07-07 11:04:21 -0700

    class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes except new instance attribute is called first name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+LockedClass module with no class or object attribute, that prevents the user
+from dynamically creating new instance attributes
+"""
+
+class LockedClass:
+    """
+    LockedClass class preventing users from creating instances
+    """
+    __user__ = ['first_name']
+
