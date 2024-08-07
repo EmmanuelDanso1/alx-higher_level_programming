9f90901fe49ad394f97faa0188fa0ecc366e74eb
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-08-07 11:49:01 -0700

    append

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // URL to fetch the movie titles
+    let url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+
+    // Make the AJAX request
+    $.getJSON(url, function(data) {
+      // Loop through each movie in the response
+      $.each(data.results, function(index, movie) {
+        // Create a new <li> element with the movie title
+        var listItem = $('<li></li>').text(movie.title);
+        // Append the <li> element to UL#list_movies
+        $('#list_movies').append(listItem);
+      });
+    });
+  });
